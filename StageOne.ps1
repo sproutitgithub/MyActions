68ed8776487015610e5eafbe5acee6498fa05130
sid local <sidlocal5269@gmail.com>
2021-03-07 22:12:29 +0000

    added invoke-command

--- StageOne.ps1
+++ StageOne.ps1
@@ -1,10 +1,13 @@
-Write-verbose "Testing to see whether i can start a service" -verbose
-$Srv = get-service BITS
-<#
+$CredsUname = "sid\djoin"
+$CredsPwd = "Pa55w0rd" | ConvertTo-SecureString -AsPlainText -Force 
+$Seccreds = New-Object System.Management.Automation.PSCredential -ArgumentList $CredsUname,$CredsPwd
+Write-verbose "Testing to see whether i can start a service"
+Invoke-Command -ComputerName DC18 -Credential $Seccreds {
+$Srv = get-service "AGPM Service"
 foreach ($item in $Srv) {
     $Srv.start()
-    $Srv1 = get-service BITS
-    IF (get-service $Srv1.status -match 'Running')
+    $Srv1 = get-service "AGPM Service"
+    IF (get-service $Srv1.Name | ? {$_.Status -match 'Running'})
     {
         Write-verbose "BITS is up and running!"
     }
@@ -12,4 +15,5 @@ foreach ($item in $Srv) {
         Write-warning "BITS isnt up and running!"
     }
 }
-#>
+
+}
